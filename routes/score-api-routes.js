ff7a8ed8c0748ad83c3df97845218b2bdd126ce6
iamjink <jinijinjin@gmail.com>
2020-04-08 10:32:03 -0700

    changes before liveshare

---
+++ routes/score-api-routes.js
@@ -0,0 +1,24 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  // Get all examples
+  app.get("/api/score", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.json(dbExamples);
+    });
+  });
+
+  // Create a new example
+  app.post("/api/score", function(req, res) {
+    db.Example.create(req.body).then(function(dbExample) {
+      res.json(dbExample);
+    });
+  });
+
+  // Delete an example by id
+  app.delete("/api/score/:id", function(req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.json(dbExample);
+    });
+  });
+};
