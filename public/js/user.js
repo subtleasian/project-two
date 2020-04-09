f9cdaf29ce46aa3dcd24b9fe6a7187f5f2003389
iamjink <jinijinjin@gmail.com>
2020-04-08 21:22:53 -0700

    user.js working for app.post

--- public/js/user.js
+++ public/js/user.js
@@ -1,21 +1,13 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // Getting jQuery references to the post body, title, form, and author select
   var profileInput = $("#inputProfile");
   var nameInput = $("#inputName");
   var emailInput = $("#inputEmail");
   var passInput = $("#inputPassword");
-  var submitButton = $("#submit");
-  // Adding an event listener for when the form is submitted
-  $(submitButton).click(function () {
-    try {
-      handleFormSubmit();
-    } catch (err) {
-      console.log("No body text in form submission");
-      threw = true;
-    }
 
-    handleFormSubmit();
-  });
+  // Adding an event listener for when the form is submitted
+
+  $(document).on("submit", "#user-form", handleFormSubmit);
 
   // A function for handling what happens when the form to create a new post is submitted
   function handleFormSubmit(event) {
@@ -30,19 +22,17 @@ $(document).ready(function () {
       alert("All fields must be completed");
     }
     // Constructing a newPost object to hand to the database
-    else {
-      submitUser({
-        picture: profileInput.val().trim(),
-        name: nameInput.val().trim(),
-        email: emailInput.val().trim(),
-        password: passInput.val().trim()
-      });
-    }
+    submitUser({
+      picture: profileInput.val().trim(),
+      name: nameInput.val().trim(),
+      email: emailInput.val().trim(),
+      password: passInput.val().trim()
+    });
   }
 
   // Submits a new post and brings user to survey page upon completion
   function submitUser(post) {
-    $.post("/api/users", post, function () {
+    $.post("/api/users", post, function() {
       console.log("new user account created.");
       window.location.href = "/survey";
     });
